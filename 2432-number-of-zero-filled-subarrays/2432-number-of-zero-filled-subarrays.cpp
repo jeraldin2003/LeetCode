c2af571ce241f9fb1e3a895e44d4034c6ea08d3f
Jeraldin P. J <113301032+jeraldin2003@users.noreply.github.com>
2025-02-13 19:23:16 +0530

    Time: 4 ms (25.56%), Space: 111.3 MB (42.71%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -7,15 +7,12 @@ public:
         for(int i=0;i<n;i++){
             if(nums[i]==0){
                 count=count+1;
+                subarray+=count;
             }
 
             else{
-                subarray+=(count*(count+1))/2;
                 count=0;
             }
-        }
-        if(count>0){
-            subarray+=(count*(count+1))/2;
         }
    return subarray;}
 };
