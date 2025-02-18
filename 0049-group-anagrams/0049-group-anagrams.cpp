ee6946ced29a3c1e1331d02493f053a1f14373bc
Jeraldin P. J <113301032+jeraldin2003@users.noreply.github.com>
2025-02-19 03:24:17 +0530

    Time: 20 ms (42.52%), Space: 26.1 MB (36.82%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,12 +1,12 @@
-#include <bits/stdc++.h>
-class Solution {
-public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>> mp;
-        for(auto x : strs){
-            string word = x;
-            sort(word.begin(),word.end());
-            mp[word].push_back(x);
+#include <bits/stdc++.h> //in this we utilized and vector of vector suppose             [1] : [1.1] [1.2] [1.3]
+class Solution {                      ////                                              [2] : [2.1] [2.2] [2.3]
+public:                                                                                 //here we can see a 2d array where elements in array is a corrsponding array
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {                        //auto x : strs iterates through strs and x is each element that it iterates through
+        unordered_map<string,vector<string>> mp;                                        //we sort each string in strs one by one. Now we push the original string to the 
+        for(auto x : strs){                                                                //corresponding sorted string inside the mp map.
+            string word = x;                                                            // for ex 1 we get                                                     
+            sort(word.begin(),word.end());                                              // aet  - {eat,tea ,ate}                                               
+            mp[word].push_back(x);                                                      //after compeleting we iterate through the map mp and push the values of each keys.
         }
         vector<vector<string>> ans;
         for(auto x : mp){
