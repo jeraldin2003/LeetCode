f852432ae9a0280c768fa8adb8cc91fcd14cbe85
Jeraldin P. J <113301032+jeraldin2003@users.noreply.github.com>
2025-05-27 20:36:41 +0530

    Time: 3 ms (59.86%), Space: 13.4 MB (15.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,10 +5,22 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        l = len(nums)
+        unique = {}
+        for i in range (0,len(nums)):
+            unique[nums[i]] = i
+
+        for i in range(0,len(nums)):
+            if target-nums[i] in unique and i!= unique[target-nums[i]]:
+                return i, unique[target-nums[i]]
+        
+
+
+
+        #BruteForce
+        '''l = len(nums)
         print(l)
         for i in range(0,l):
             for j in range(i+1,l):
                 if nums[i] + nums[j] == target:
-                    return i,j
+                    return i,j '''
 
